184593a26ee557d6838e1d6fea12eabe01316312
Marco Prado <maprado@microsoft.com>
2019-10-22 11:39:24 -0600

    commit new branch

--- BikeSharingApp/Bikes/server.js
+++ BikeSharingApp/Bikes/server.js
@@ -231,7 +231,7 @@ app.get('/api/bikes/:bikeId', function(req, res) {
         var theBike = result;
 
         // Hard code image url *FIX ME* -> comment line 234
-        theBike.imageUrl = "/static/logo.svg";
+        // theBike.imageUrl = "/static/logo.svg";
         
         theBike.id = theBike._id;
         delete theBike._id;
